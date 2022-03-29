4786c20a3b86145bea3bae351460b41e28180d82
Anson_kyk <anson.kong@mail.utoronto.ca>
2022-03-29 13:09:02 -0400

    two goal met

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const cleardone=document.querySelector("#cdone");
 const items=document.querySelector("#allitem");
 const newiteminput=document.querySelector("#newiteminput");
 let count=0
-
+let completeditem=[]
 //Always event listener
 clearallbutton.addEventListener("click",function(){
     while (items.firstChild) {
@@ -45,14 +45,30 @@ form.addEventListener("submit",function(event){
     newitem.appendChild(deleter)
     //Add event listener
     complete.addEventListener("click",function(){
+        if (completeditem.includes(complete.id)){
+            var temp=complete.id;
+            var num=temp.replace(/\D/g,'')
+            var temp3=document.querySelector("#item"+num);
+            
+            const index = completeditem.indexOf(complete.id);
+            if (index > -1) {
+                completeditem.splice(index, 1); // 2nd parameter means remove one item only
+            }
+
+            var temp2=document.querySelector("#itemtext"+num);
+            temp2.style.textDecoration= ''
+            temp3.classList.remove("Complete");
+        }else{
         var temp=complete.id;
-        
+        completeditem.push(temp);
         var num=temp.replace(/\D/g,'');
         console.log(num);
         var temp2=document.querySelector("#itemtext"+num);
         temp2.style.textDecoration= 'line-through'
         var temp3=document.querySelector("#item"+num);
         temp3.classList.add('Complete');
+        }
+
     })
     deleter.addEventListener("click",function(){
         var temp=complete.id;
